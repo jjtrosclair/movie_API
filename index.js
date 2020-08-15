47849ea6b8e0a635e374b605f4a88e7fbb80052a
jjtrosclair <57603010+jjtrosclair@users.noreply.github.com>
2020-08-15 15:20:41 +0200

    fixed error typo

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.post("/users",
 (req, res) => {
 //checks the validation object for errors
 let errors = validationResult(req);
-if (!error.isEmpty()) {
+if (!errors.isEmpty()) {
     return res.status(422).json({errors: errors.array()});
 }
 
