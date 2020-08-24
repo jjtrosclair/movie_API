b56c6720e4722ac22476bf5e9974be918f29c28b
jjtrosclair <57603010+jjtrosclair@users.noreply.github.com>
2020-08-24 08:17:54 +0200

    removed authentication on GET all movies

--- index.js
+++ index.js
@@ -50,9 +50,7 @@ app.get('/', (req, res) => {
 
 // Gets the list of data about ALL movies
 
-app.get("/movies", passport.authenticate('jwt', {
-  session: false
-}), (req, res) => {
+app.get("/movies", (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
