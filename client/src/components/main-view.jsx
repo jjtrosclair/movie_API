531b7c8fa56e2d6bc25699efd911f57f9873d6f3
jjtrosclair <57603010+jjtrosclair@users.noreply.github.com>
2020-08-25 08:48:46 +0200

    added client file

---
+++ client/src/components/main-view.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import axios from "axios";
+
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+
+export class MainView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: null,
+      selectedMovie: null,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("<https://my-flix-app-4455.herokuapp.com/movies>")
+      .then((response) => {
+        //assign result to the state
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie,
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    // before movies have loaded
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {selectedMovie ? (
+          <MovieView movie={selectedMovie} />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onClick={(movie) => this.onMovieClick(movie)}
+            />
+          ))
+        )}
+      </div>
+    );
+  }
+}
---
+++ client/src/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { MainView } from "./components/main-view";
+
+//import statement indicating that you need to bundle this file
+
+import "./index.scss";
+
+//main component that will eventually use all others
+
+class MyFlixApplication extends React.Component {
+  render() {
+    return <MainView />;
+  }
+}
+
+//finds the root of your app
+
+const container = document.getElementsByClassName("app-container")[0];
+
+// tells React to render your app in the root DOM element
+
+ReactDOM.render(React.createElement(MyFlixApplication), container);
