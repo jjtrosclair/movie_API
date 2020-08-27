18617817fa3c0303fca0a96402f7b4b4f92befb7
jjtrosclair <57603010+jjtrosclair@users.noreply.github.com>
2020-08-27 09:57:38 +0200

    added login-view

--- client/src/components/main-view.jsx
+++ client/src/components/main-view.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 
+import { LoginView } from "./login-view/login-view";
 import { MovieCard } from "./movie-card";
 import { MovieView } from "./movie-view";
 
@@ -11,6 +12,7 @@ export class MainView extends React.Component {
     this.state = {
       movies: null,
       selectedMovie: null,
+      user: null,
     };
   }
 
@@ -34,8 +36,17 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
   render() {
     const { movies, selectedMovie } = this.state;
+
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
 
     // before movies have loaded
     if (!movies) return <div className="main-view" />;
